144f36b07269bb2ca1291c52b2645e7c2d667116
sh-analytics1 <115117833+sh-analytics1@users.noreply.github.com>
2022-10-06 03:33:28 +0200

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-
-const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
-
-tl.to('.text', {y: "0%", duration: 1, stagger: 0.25 });
-tl.to('.intro', {y: "-100%", duration: 0.8, delay: 0.5} );
-
-
-
