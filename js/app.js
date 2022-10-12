6cd7fac4d546b71d05eeb820b726cf4f7c4f1583
sh-analytics1 <115117833+sh-analytics1@users.noreply.github.com>
2022-10-13 00:55:51 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 
+// hide scroll down button on scroll
+const scrollBtn = document.querySelector(".scroll-button");
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+    if (lastScrollY < window.scrollY) {
+        scrollBtn.classList.add("scroll--hidden");
+    } else {
+        scrollBtn.classList.remove("scroll--hidden");
+    }
+        
+    lastScrollY = window.scrollY;
+});
+
